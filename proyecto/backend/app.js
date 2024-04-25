a5b696dafd710c09589f3cfe2cfe11ec3f200dc2
JessiLuque <159462671+JessiLuque@users.noreply.github.com>
2024-04-25 10:52:28 -0700

    proyecto - conexión a mysql

--- proyecto/backend/app.js
+++ proyecto/backend/app.js
@@ -6,10 +6,40 @@ let app = express();
 app.use(express.json());
 app.use(cors());
 
+//Conexión a MySQL
+let conexion = mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    password: '',
+    database: 'pwdata',
+    port: '3306'
+});
+
+//Nos conectamos a MySQL
+conexion.connect(function(error){
+    if(error){
+        throw error;
+    }else{
+        console.log('Conectado a la base de datos');
+    }
+});
+
 //rutas de acceso
 app.get("/", function(req,res){
-    res.send("Rutassssss");
+    res.send("Ruta de inicio");
 })
+
+
+//seleccionamos todos los clientes
+app.get('/api/clientes', (req,res)=>{
+    conexion.query('SELECT * FROM Clientes',(error, filas)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(filas);
+        }
+    });
+});
 
 //Encender el servidor
 let puerto = 3000;
